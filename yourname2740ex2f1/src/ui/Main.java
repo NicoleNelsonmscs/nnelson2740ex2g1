f7a450d70097e8cb84dd4d411c4b248062f06447
mswanson-mscst <mswanson@southeastmn.edu>
2018-02-27 15:12:40 -0600

    Added Ch17FutureValueFXML

---
+++ yourname2740ex2f1/src/ui/Main.java
@@ -0,0 +1,104 @@
+package ui;
+
+import business.Ex2fSample;
+
+public class Main {
+    public static void main(String[] args) {
+        Ex2fSample obj1;
+
+//        obj1 = new Ex2fSample(0, 2, 3.0, 4.0, "Aaaa", "Bbbb");
+//        System.out.println(obj1.test1());
+//
+//        obj1.setI1(1);
+//        System.out.println(obj1.test1());
+//
+//        obj1.setI1(10);
+//        System.out.println(obj1.test1());
+//
+//        obj1.setI1(11);
+//        System.out.println(obj1.test1());
+//
+//        obj1 = new Ex2fSample(1, 1, 3.0, 4.0, "Aaaa", "Bbbb");
+//        System.out.println(obj1.test2());
+//
+//        obj1.setI1(2);
+//        System.out.println(obj1.test2());
+//
+//        obj1.setI1(3);
+//        System.out.println(obj1.test2());
+//
+//        obj1 = new Ex2fSample(1, 2, 3.0, 4.0, "Aaaa", "Bbbb");
+//        System.out.println(obj1.test3());
+//
+//        obj1.setS1("Bbbb");
+//        System.out.println(obj1.test3());
+//
+//        obj1.setS1("Bbb");
+//        System.out.println(obj1.test3());
+//
+//        obj1.setS1("C");
+//        System.out.println(obj1.test3());
+//
+//        obj1.setS1("Bbbb ");
+//        System.out.println(obj1.test3());
+//
+//        obj1.setS1("99");
+//        obj1.setS2("100");
+//        System.out.println(obj1.test3());
+//
+//        obj1 = new Ex2fSample(1, 2, 0, 7.50, "Aaaa", "Bbbb");
+//        System.out.println(obj1.test4());
+//
+//        obj1.setD1(168.1);
+//        System.out.println(obj1.test4());
+//
+//        obj1.setD1(40.0);
+//        System.out.println(obj1.test4());
+//
+//        obj1.setD2(100.0);
+//        System.out.println(obj1.test4());
+//
+//        obj1.setD2(10);
+//        System.out.println(obj1.test4());
+//
+//        obj1.setD1(50);
+//        System.out.println(obj1.test4());
+//
+//        obj1 = new Ex2fSample(1, 2, 0, 7.50, "Aaaa", "Bbbb");
+//        System.out.println(obj1.test5());
+//
+//        obj1.setD1(168.1);
+//        System.out.println(obj1.test5());
+//
+//        obj1.setD1(40.0);
+//        System.out.println(obj1.test5());
+//
+//        obj1.setD2(100.0);
+//        System.out.println(obj1.test5());
+//
+//        obj1.setD2(10);
+//        System.out.println(obj1.test5());
+//
+//        obj1.setD1(50);
+//        System.out.println(obj1.test5());
+//
+//        obj1 = new Ex2fSample();
+//        System.out.println(obj1.test6());
+//
+//        obj1.setS1("");
+//        System.out.println(obj1.test6());
+//
+//        obj1.setS1("aaaa ");
+//        System.out.println(obj1.test6());
+//
+//        obj1.setS1("dddd");
+//        System.out.println(obj1.test6());
+//
+//        obj1.setS1("aaaa");
+//        System.out.println(obj1.test6());
+//
+//        obj1.setS1("cccc");
+//        System.out.println(obj1.test6());
+
+    }
+}
