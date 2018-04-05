83170e2b00571bea9933abea1b4def9a5643faec
mswanson-mscst <mswanson@southeastmn.edu>
2018-04-05 15:14:47 -0500

    Added Ex3b1

---
+++ yourname2740ex3b1/src/business/Values.java
@@ -0,0 +1,26 @@
+package business;
+
+public class Values {
+    private float[] values = {
+            75.2f, 73.4f, 75.2f, 73.4f, 75.2f, 73.4f, 75.2f, 73.4f, 71.6f, 73.4f,
+            71.6f, 73.4f, 71.6f, 73.4f, 71.6f, 73.4f, 75.2f, 77f, 78.8f, 80.6f,
+            82.4f, 84.2f, 82.4f, 84.2f, 82.4f, 84.2f, 86f, 87.8f, 86f, 84.2f,
+            82.4f, 80.6f, 82.4f, 80.6f, 82.4f, 80.6f, 82.4f, 80.6f, 78.8f, 77f,
+            78.8f, 77f, 75.2f, 77f, 75.2f, 73.4f, 75.2f, 73.4f, 75.2f, 73.4f};
+
+    public float calcTotal() {
+        return 0.0f;
+    }
+
+    public float calcMin() {
+        return 0.0f;
+    }
+
+    public float calcMax() {
+        return 0.0f;
+    }
+
+    public float calcAvg() {
+        return 0.0f;
+    }
+}
