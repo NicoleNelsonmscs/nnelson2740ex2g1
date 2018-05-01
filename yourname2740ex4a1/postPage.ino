4ae948e524c973ad3c1523a12adab5ba5ee76c3a
mswanson-mscst <mswanson@southeastmn.edu>
2018-05-01 10:07:25 -0500

    Added sample Arduino sketch: yourname2740ex4a1, DHT11 Ethernet

---
+++ yourname2740ex4a1/postPage.ino
@@ -0,0 +1,54 @@
+// --------------------------------- postPage()  ---------------------------------
+// ---------------------- Performs http Post of json payload ---------------------
+byte postPage(char* json)
+{
+  int inChar;
+  Serial.println("postPage() connecting...");
+
+  if (client.connect(serverIp, serverPort)) {
+    Serial.println("postPage() connected");
+
+    // send http header
+    client.println("POST /api/sensorreadings HTTP/1.1");
+    client.println("Host: 204.77.51.249"); // or generate from your server variable to not hardwire
+    client.println("User-Agent: Arduino/uno ethernet");
+    client.println("Connection: close");
+    client.println("Content-Type: application/json");
+    client.print("Content-Length: ");
+    client.println(strlen(json));// number of bytes in the payload
+    client.println();// important: need an empty line here
+    // send payload
+    client.println(json);
+  }
+  else
+  {
+    Serial.println(F("postPage() connect failed"));
+    return 0;
+  }
+
+  int connectLoop = 0;
+
+  while(client.connected())
+  {
+    while(client.available())
+    {
+      inChar = client.read();
+      Serial.write(inChar);
+      connectLoop = 0;
+    }
+
+    delay(1);
+    connectLoop++;
+    if(connectLoop > 10000)
+    {
+      Serial.println();
+      Serial.println(F("Timeout"));
+      client.stop();
+    }
+  }
+
+  Serial.println();
+  Serial.println(F("disconnecting."));
+  client.stop();
+  return 1;
+}
